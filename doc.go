ff5a73eea61dbec4555829abb20dc2e881894063
Coby Benveniste <coby.benveniste@gmail.com>
2023-11-19 13:04:15 +0200

    Doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,4 @@
-// TODO: Write some doc comments.
+// Package staticsyncpool provides a wrapper around the Go Standard Library sync.Pool primitive,
+// which ensures ensures that we keep a static amount of values in memory (unaffected by GC),
+// while allowing us the full benefits of the sync.Pool.
 package staticsyncpool
